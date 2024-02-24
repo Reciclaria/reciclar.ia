2f122bf841a7f7991c0a020883fd1059390b67f4
Luis Leao <luis.leao@gmail.com>
2024-02-24 12:57:09 -0300

    base cloud function

--- functions/index.js
+++ functions/index.js
@@ -13,7 +13,12 @@ const logger = require("firebase-functions/logger");
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.identificaLixo = onRequest((request, response) => {
+
+    logger.info("Hello logs!", {structuredData: true});
+    logger.info('CONTEUDO DA REQUISICAO', request.body);
+    // TODO: verificar se retornou imagem
+    // TODO: 
+    response.send("Hello from Firebase!");
+
+});
